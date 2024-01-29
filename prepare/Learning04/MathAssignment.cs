2eb3e124aaf5fb661ce8bb08f59b126bd77ad11a
AnastasiyaShvydko <84057111+AnastasiyaShvydko@users.noreply.github.com>
2024-01-29 11:06:30 -0500

    week4

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,21 @@
+using System.Reflection.Metadata.Ecma335;
+
+public class MathAssignment : Assignment{
+    string _textbookSection = "";
+    string _problems = "";
+
+ 
+public MathAssignment(string studentName, string topic, string textbookSection , string problems) : base ( studentName,topic )
+{
+    _textbookSection = textbookSection;
+    _problems = problems;
+
+}
+
+public string GetHomeworkList(){
+    return $"{_textbookSection}{_problems}";
+}
+
+
+
+    }
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+       // Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assignment1 = new Assignment("Brenet Andrew", "Miltiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Anna V", "Subtraction", "Section 9-12","Problems 5-9");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+
+        WritingAssignment assignment3 = new WritingAssignment("Julia Ley", "Essay", "My favorite day");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInfromation());
     }
 }
